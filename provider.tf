6b7b040bd5f6b9d272fa22e866899690927f6c05
Harish-630 <harishkommu01@gmail.com>
2025-06-26 16:32:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0.0"  # ✅ Fully compatible with vpc 3.14.2
+      version = "~> 5.95.0"  # ✅ Fully compatible with vpc 3.14.2
     }
   }
 }
