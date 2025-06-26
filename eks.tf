e4f718b2ab1751cf237601fb0a2ee670e7b49b6b
Harish-630 <harishkommu01@gmail.com>
2025-06-26 16:29:34 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.0.0"
+  version = "20.37.1"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
